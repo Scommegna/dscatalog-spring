9346764a654298f5b188d2fb403001caee9581d2
scommegna <scommegnal@hotmail.com>
2023-11-06 14:17:34 -0300

    feat: Not found exception

--- backend/src/main/java/com/scommegna/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/scommegna/dscatalog/services/CategoryService.java
@@ -3,12 +3,14 @@ package com.scommegna.dscatalog.services;
 import com.scommegna.dscatalog.dto.CategoryDTO;
 import com.scommegna.dscatalog.entities.Category;
 import com.scommegna.dscatalog.repositories.CategoryRepository;
+import com.scommegna.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,5 +26,14 @@ public class CategoryService {
 
 
         return listDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found."));
+
+        return new CategoryDTO(entity);
     }
 }
