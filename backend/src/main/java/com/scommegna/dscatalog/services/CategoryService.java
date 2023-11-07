3b6014d832c21f26fc0c02e2cd4e62773eaaf4db
scommegna <scommegnal@hotmail.com>
2023-11-07 10:47:07 -0300

    refactor: list all categories by pagination

--- backend/src/main/java/com/scommegna/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/scommegna/dscatalog/services/CategoryService.java
@@ -9,6 +9,8 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,13 +24,10 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-        List<Category> list = repository.findAll();
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> list = repository.findAll(pageRequest);
 
-        List<CategoryDTO> listDTO = list.stream().map(cat -> new CategoryDTO(cat)).collect(Collectors.toList());
-
-
-        return listDTO;
+        return list.map(cat -> new CategoryDTO(cat));
     }
 
     @Transactional(readOnly = true)
