0c9417a448a982351d43af9a46267c8262bf586e
scommegna <scommegnal@hotmail.com>
2023-11-07 09:49:40 -0300

    refactor: dto timestamp

--- backend/src/main/java/com/scommegna/dscatalog/entities/Category.java
+++ backend/src/main/java/com/scommegna/dscatalog/entities/Category.java
@@ -3,6 +3,7 @@ package com.scommegna.dscatalog.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -13,6 +14,12 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Category() {
 
@@ -31,12 +38,30 @@ public class Category implements Serializable {
         this.id = id;
     }
 
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
